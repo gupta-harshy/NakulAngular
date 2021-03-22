df7fe5413e50d896ab3433da00c555a91a66ea71
44775 <44775@BCCX1Y3-44775>
2021-03-22 20:20:45 +0530

    All changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/modals/user';
+import { AppService } from './app.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'contacts';
+export class AppComponent implements OnInit{
+  users$: Observable<User[]> | undefined;
+
+  constructor(private appsService: AppService){}
+
+  ngOnInit(){
+    this.users$ = this.appsService.getContacts();
+
+  }
+
+
+
 }
